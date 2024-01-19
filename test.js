37357a56e294c89b92ccdde5a73aeac9c84d6ddb
Бобылев Дмитрий <netlight4me@gmail.com>
2024-01-19 23:53:07 +0300

    WIP

--- test.js
+++ test.js
@@ -20,37 +20,60 @@ x =
 }
 
 vals = []
-vals[0] = ['$.client','string','123']
-vals[1] = ['$.date','string','01.01.2024']
-vals[2] = ['$.car', 'object','']
-vals[3] = ['$.car.model', 'string','kia']
-vals[4] = ['$.car.vin', 'string','xxxzzzyyy']
-vals[5] = ['$.car.risks', 'list','']
-vals[6] = ['$.car.risks[0]', 'object','']
-vals[7] = ['$.car.risks[0].x', 'string','123']
-vals[8] = ['$.car.risks[0].y', 'string','567']
-vals[7] = ['$.car.risks[1]', 'object', '']
-vals[7] = ['$.car.risks[1].x', 'string','123']
-vals[8] = ['$.car.risks[1].y', 'string','567']
-vals[9] = ['$.car.conds', 'list','']
-vals[10] = ['$.car.conds[0]', 'string','agent']
-vals[11] = ['$.car.conds[1]', 'string','lox']
+vals[0] = ['client','string','123']
+vals[1] = ['date','string','01.01.2024']
+vals[2] = ['car', 'object','']
+vals[3] = ['car.model', 'string','kia']
+vals[4] = ['car.vin', 'string','xxxzzzyyy']
+vals[5] = ['car.risks', 'list','']
+vals[6] = ['car.risks[0]', 'object','']
+vals[7] = ['car.risks[0].x', 'string','123']
+vals[8] = ['car.risks[0].y', 'string','567']
+vals[7] = ['car.risks[1]', 'object', '']
+vals[7] = ['car.risks[1].x', 'string','123']
+vals[8] = ['car.risks[1].y', 'string','567']
+vals[9] = ['car.conds', 'list','']
+vals[10] = ['car.conds[0]', 'string','agent']
+vals[11] = ['car.conds[1]', 'string','lox']
 
 obj = {}
 
 
 
-function getobj(l = []){
-    if (length(l) === 1 && l[0] === '$')
-    
-    return 
+function getobj(pobj, l=[]){
+    co
+
+    if (l.length === 1)
+    {
+        return pobj;
+    }
+    else
+    {
+        s = l.shift();
+        return getobj(pobj[s], l)
+    }
 }
 
-for(let i=0;i<12;i++){
+for(let i=0;i<8;i++){
 
-  if (vals[i][1] === 'object'){
-    
-  }
+    let path = vals[i][0].split(/\[|\]\.|\./);
+    let type = vals[i][1];
+    let value = vals[i][2];
 
-  console.log(vals[i][0].split('.'))
+    if (type === 'string'){
+      o = getobj(obj, path);
+      o[path[path.length-1]] = value;
+    }
+
+    if (type === 'object'){
+        o = getobj(obj, path);
+        o[path[path.length-1]] = {};
+    }
+
+    if (type === 'list'){
+        o = getobj(obj, path);
+        o[path[path.length-1]] = [];
+    }
 }
+
+console.log(JSON.stringify(obj,null,2))
